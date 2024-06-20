681a41f56c359ca8760cf8c58c3e3ca8a98eb489
Maseh01 <masehvincent@outlook.com>
2024-06-21 00:25:18 +0300

    added files to project-7

---
+++ project-7/src/components/Testimonials/TestimonialBoxes.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import TestimonialBox from "./TestimonialBox";
+
+function TestimonialBoxes() {
+  return (
+    <>
+      {/* <TestimonialBox
+        text=" “I've been a member of Nguvu Gym for the past 6 months and it has been an amazing experience. The trainers are knowledgeable and supportive, the
+                  equipment is top-notch, and the community of members is friendly and
+                  encouraging.”"
+        name="John Wick"
+        job="Startbucks Employee"
+      /> */}
+      <TestimonialBox
+        text=" “I've been a member of Nguvu Gym for the past 6 months and it has been an amazing experience. The trainers are knowledgeable and supportive, the
+                  equipment is top-notch, and the community of members is friendly and
+                  encouraging.”"
+        name="Billy Jean"
+        job="CEO of Hogwarts"
+      />
+      {/* <TestimonialBox
+        text=" “I've been a member of Nguvu Gym for the past 6 months and it has been an amazing experience. The trainers are knowledgeable and supportive, the
+                  equipment is top-notch, and the community of members is friendly and
+                  encouraging.”"
+        name="Gill Bates"
+        job="Founder of Microfost"
+      /> */}
+    </>
+  );
+}
+
+export default TestimonialBoxes;
